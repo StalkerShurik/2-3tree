0b8a8039d67c1b21e1735f205e0952bed9e969e7
StalkerShurik <AlexanderGaponov@outlook.com>
2022-02-14 00:08:05 +0300

    commit

---
+++ cmake-build-debug/CMakeFiles/23Tree.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "23Tree"
+  "23Tree.pdb"
+  "CMakeFiles/23Tree.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/23Tree.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
